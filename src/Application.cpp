e64b44a27f88b05565bd8758868b07fd497c6d58
harshit <harshitrock111@gmail.com>
2019-10-20 01:41:38 +0530

    glew

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <GL/glew.h>
+#include <iostream>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -20,6 +21,13 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "ERROR" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
@@ -31,6 +39,7 @@ int main(void)
 		glVertex2f(0.0f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
