84bb087f46df70ad7dc3e9d67e1b92de8c0a9e25
harshit <harshitrock111@gmail.com>
2019-11-17 00:40:50 +0530

    SHD UD

--- SELF/SHADER/Src/main.cpp
+++ SELF/SHADER/Src/main.cpp
@@ -2,7 +2,60 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_WIDTH = 600;
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout (location=0) in vec3 aPos;\n"
+"layout (location=1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"gl_Position =vec4 (aPos,1.0);\n"
+"ourColor = aColor;\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"FragColor=vec4(ourColor,1.0f);\n"
+"}\n\0";
+
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif // __APPLE__
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "SHADERES", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+
 }
