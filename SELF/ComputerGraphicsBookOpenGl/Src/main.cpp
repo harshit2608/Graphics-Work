36ab9465d8cb74810d4abf61871277a3d8dd1646
harshit <harshitrock111@gmail.com>
2020-06-01 01:00:53 +0530

    ComputerGraphicsBookopenGL

--- SELF/ComputerGraphicsBookOpenGl/Src/main.cpp
+++ SELF/ComputerGraphicsBookOpenGl/Src/main.cpp
@@ -6,12 +6,11 @@
 float cameraX, cameraY, cameraZ;
 float cubeLocX, cubeLocY, cubeLocZ;
 
-unsigned int renderingProgram;
+unsigned int renderingProgram, mvLoc, projLoc, vLoc;
 unsigned int vao[VAO];
 unsigned int vbo[VBO];
-unsigned int mvLoc, projLoc;
-float aspect;
-glm::mat4 pMat, vMat, mMat, mvMat;
+float aspect, tf;
+glm::mat4 pMat, vMat, mMat, mvMat, rMat, tMat;
 
 void setupVertices(void);
 void init(GLFWwindow* window);
@@ -98,7 +97,7 @@ void init(GLFWwindow* window)
 	renderingProgram = Shader::createShaderProgram("Res/Shaders/Vertex.glsl", "Res/Shaders/Fragment.glsl");
 	cameraX = 0.0f;
 	cameraY = 0.0f;
-	cameraZ = 8.0f;
+	cameraZ = 25.0f;
 	cubeLocX = 0.0f;
 	cubeLocY = -2.0f;
 	cubeLocZ = 0.0f;
@@ -108,7 +107,7 @@ void init(GLFWwindow* window)
 void display(GLFWwindow* window, double currentTime)
 {
 	glClear(GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0f, 0.5f, 0.25f, 1.0f);
+	//glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(renderingProgram);
@@ -121,17 +120,35 @@ void display(GLFWwindow* window, double currentTime)
 
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
-	mvMat = vMat * mMat;
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
+	for (int i = 0; i < 25; i++)
+	{
+		tf = currentTime + i;
+		//new code 1
+		tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(0.35f * tf) * 8.0f, cos(0.52f * tf) * 8.0f, sin(0.7f * tf) * 8.0f));
+		rMat = glm::rotate(glm::mat4(1.0f), 1.75f * tf, glm::vec3(0.0f, 1.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(1.0f, 0.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(0.0f, 0.0f, 1.0f));
+		mMat = tMat * rMat;
+		//new code 1 end
 
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+		mvMat = vMat * mMat;
+
+		glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+		//glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+		//timeFactor = ((float)currentTime);
+		//tfLoc = glGetUniformLocation(renderingProgram, "tf");
+		//glUniform1f(tfLoc, (float)timeFactor);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
+
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawArraysInstanced(GL_TRIANGLES, 0, 36,24);
+	}
 }
 
 void processInput(GLFWwindow* window)
